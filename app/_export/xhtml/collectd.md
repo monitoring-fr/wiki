280fbc2c02ce9b2fec710ef5d88a4a4f37e54d5b
Olivier JAN <olivierjan.com@gmail.com>
2015-04-05 14:49:18 +0200

    clean: unused files

--- app/_export/xhtml/collectd.md
+++
@@ -1,6 +0,0 @@
----
-layout: page
----
-
-Voir
-[Collectd](../../nagios/integration/collectd.html "nagios:integration:collectd")
--- app/lib/exe/opensearch.php
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0"?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-  <ShortName>wiki monitoring-fr.org</ShortName>
-  <Image width="16" height="16" type="image/x-icon">http://wiki.monitoring-fr.org/lib/tpl/arctic/images/favicon.ico</Image>
-  <Url type="text/html" template="http://wiki.monitoring-fr.org/doku.php?do=search&amp;id={searchTerms}" />
-  <Url type="application/x-suggestions+json" template="http://wiki.monitoring-fr.org/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
-</OpenSearchDescription>
